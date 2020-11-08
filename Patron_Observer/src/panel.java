d09e420a869e8b051d3dc638ad0289ab0c729fee
edisonrmedina <edison1282015@gmail.com>
2020-11-08 01:17:04 -0500

    modificacion Para uso de patron obervador

--- Patron_Observer/src/panel.java
+++ Patron_Observer/src/panel.java
@@ -1,18 +1,28 @@
 
 	import java.awt.*;
 	import java.awt.event.*;
-	import javax.swing.*;
+import java.util.ArrayList;
 
-public class panel extends JPanel {
+import javax.swing.*;
 
-	private JButton btn1;
-	private JButton btn2;
-	private JButton btn3;
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
+
+public class panel extends JPanel implements observable  {
+
+	private User btn1;
+	private User btn2;
+	private User btn3;
+	private ArrayList<Observer> users = new ArrayList<Observer>();
+	
 	
 	public panel() {
-		btn1 = new JButton("Amarillo");
-		btn2 = new JButton("Rosa");
-		btn3 = new JButton("Verde");
+		btn1 = new User("Amarillo");	
+		users.add(btn1);
+		btn2 =new User("Rosa");
+		users.add(btn2);
+		btn3 = new User("Verde");
+		users.add(btn3);
 		
 		add(btn1);
 		add(btn2);
@@ -36,16 +46,33 @@ public class panel extends JPanel {
               if (buttObj == btn1) {
             	  setBackground(color1);
             	  System.out.println(nombresColores.COLORESCONNOMBRE.get(color1));
+            	  notifyObserver(nombresColores.COLORESCONNOMBRE.get(color1),btn1);
               }else if(buttObj == btn2) {
             	  setBackground(color2);
             	  System.out.println(nombresColores.COLORESCONNOMBRE.get(color2));
+            	  notifyObserver(nombresColores.COLORESCONNOMBRE.get(color2),btn2);
               }else {
             	  setBackground(color3);
             	  System.out.println(nombresColores.COLORESCONNOMBRE.get(color3));
+            	  notifyObserver(nombresColores.COLORESCONNOMBRE.get(color3),btn3);
               }
 				  
 				  
 	      }//end of Action
 	
 }
+
+	@Override
+	public void notifyObserver(String color, Object user) {
+		
+		for (Observer obs: users) {
+			if (obs == user ) {
+				obs.informar(color);
+			}
+		}
+		
+	}
+
+
+	
 }
